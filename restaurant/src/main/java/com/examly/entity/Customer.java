0f01d21cf280af2a988d30143a447f2400676064
neojarvis <techadmin@iamneo.ai>
2025-05-09 07:27:34 +0000

    Manual-save-commit-09/05/2025--12:57:34

--- restaurant/src/main/java/com/examly/entity/Customer.java
+++ restaurant/src/main/java/com/examly/entity/Customer.java
@@ -18,5 +18,17 @@ public class Customer {
     }
 
     public int getCustomerId(){return customerId;}
-    public void setCustomerId(int)
+    public void setCustomerId(int customerId){this.customerId=customerId;}
+
+    public String getName(){return name;}
+    public void setName(String name){this.name=name;}
+
+    public String getEmail(){return email;}
+    public void setEmail(String email){this.email=email;}
+
+    public String getPhoneNumber(){return phoneNumber;}
+    public void setPhoneNumber(String phoneNumber){this.phoneNumber=phoneNumber;}
+
+    public String getPassword(){return password;}
+    public void setPassword(String password){this.password=password;}
 }
