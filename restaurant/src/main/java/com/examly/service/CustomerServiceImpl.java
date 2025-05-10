cb60ef1f859a54ac26d2921c58fdf4ddd1460215
neojarvis <techadmin@iamneo.ai>
2025-05-10 12:16:55 +0000

    Manual-save-commit-10/05/2025--17:46:54

--- restaurant/src/main/java/com/examly/service/CustomerServiceImpl.java
+++ restaurant/src/main/java/com/examly/service/CustomerServiceImpl.java
@@ -15,12 +15,27 @@ public class CustomerServiceImpl implements CustomerService{
         String checkQuery = "SELECT COUNT(*) FROM customer WHERE email=?";
         String insertQuery = "INSERT INTO customer (name, email, phoneNumber, password) VALUES(?,?,?,?)";
 
-        try(Connection conn = DBConnectionUtil.getConnection());
+        try(Connection conn = DBConnectionUtil.getConnection();
            PreparedStatement checkStmt = conn.prepareStatement(checkQuery);
-           PreparedStatement insertStmt = conn.prepareStatement(insertQuery);
+           PreparedStatement insertStmt = conn.prepareStatement(insertQuery)){ 
 
            checkStmt.setString(1,customer.getEmail());
-           ResultSet 
+           ResultSet rs = checkStmt.executeQuery();
+
+           if(rs.next() && rs.getInt(1) >0){
+            throw new EmailAlreadyRegisteredException("Email "+ customer.getEmail() + "is already registered");
+           }
+
+           insertStmt.setString(1, customer.getName());
+           insertStmt.setString(2, customer.getEmail());
+           insertStmt.setString(3, customer.getPhoneNumber());
+           insertStmt.setString(4, customer.getPassword());
+
+           return insertStmt.executeUpdate() > 0;
+    }catch(SQLException e){
+        e.printStackTrace();
+        return false;
     }
-    
+}
+
 }
