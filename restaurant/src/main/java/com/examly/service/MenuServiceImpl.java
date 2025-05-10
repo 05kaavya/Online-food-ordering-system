489f0f53a711e58af584e75b7ba5ca162ce18e6a
neojarvis <techadmin@iamneo.ai>
2025-05-10 12:22:20 +0000

    Auto-save-commit-10/05/2025--17:52:18

--- restaurant/src/main/java/com/examly/service/MenuServiceImpl.java
+++ restaurant/src/main/java/com/examly/service/MenuServiceImpl.java
@@ -1,5 +1,42 @@
 package com.examly.service;
 
-public class MenuServiceImpl {
-    
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.examly.entity.Customer;
+import com.examly.exception.EmailAlreadyRegisteredException;
+import com.examly.util.DBConnectionUtil;
+
+public class MenuServiceImpl implements MenuService{
+    @Override
+    public boolean createCustomer(Customer customer) throws EmailAlreadyRegisteredException{
+        String checkRestaurant = "SELECT COUNT(*) FROM  WHERE email=?";
+        String insertQuery = "INSERT INTO customer (name, email, phoneNumber, password) VALUES(?,?,?,?)";
+
+        try(Connection conn = DBConnectionUtil.getConnection();
+           PreparedStatement checkStmt = conn.prepareStatement(checkQuery);
+           PreparedStatement insertStmt = conn.prepareStatement(insertQuery)){ 
+
+           checkStmt.setString(1,customer.getEmail());
+           ResultSet rs = checkStmt.executeQuery();
+
+           if(rs.next() && rs.getInt(1) >0){
+            throw new EmailAlreadyRegisteredException("Email "+ customer.getEmail() + "is already registered");
+           }
+
+           insertStmt.setString(1, customer.getName());
+           insertStmt.setString(2, customer.getEmail());
+           insertStmt.setString(3, customer.getPhoneNumber());
+           insertStmt.setString(4, customer.getPassword());
+
+           return insertStmt.executeUpdate() > 0;
+    }catch(SQLException e){
+        e.printStackTrace();
+        return false;
+    }
 }
+
+}
+
