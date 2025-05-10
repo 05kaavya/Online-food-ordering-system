4f0211d141d21cb0088ea7bb8a9ccf63c27b5469
neojarvis <techadmin@iamneo.ai>
2025-05-10 13:52:04 +0000

    Manual-save-commit-10/05/2025--19:22:03

--- restaurant/src/main/java/com/examly/service/PaymentServiceImpl.java
+++ restaurant/src/main/java/com/examly/service/PaymentServiceImpl.java
@@ -1,5 +1,31 @@
 package com.examly.service;
 
-public class PaymentServiceImpl {
-    
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+import com.examly.entity.Payment;
+import com.examly.util.DBConnectionUtil;
+
+public class PaymentServiceImpl implements PaymentService{
+    @Override
+    public boolean processPayment(Payment payment) {
+        String insertQuery = "INSERT INTO payment (paymentId, orderId, paymentDate, paymentStatus, amountPaid) VALUES(?, ?, ?, ?, ?)";
+
+        try(Connection conn = DBConnectionUtil.getConnection();
+           PreparedStatement stmt = conn.prepareStatement(insertQuery)){
+           
+           stmt.setInt(1, payment.getPaymentId());
+           stmt.setInt(2, payment.getOrderId());
+           stmt.setTimestamp(3, new java.sql.Timestamp(payment.getPaymentDate().getime()));
+           stmt.setString(4, payment.getPaymentStatus());
+           stmt.setDouble(5, payment.getAmountPaid());
+
+           return stmt.executeUpdate() > 0;
+    }catch(SQLException e){
+        e.printStackTrace();
+        return false;
+    }
+}
 }
