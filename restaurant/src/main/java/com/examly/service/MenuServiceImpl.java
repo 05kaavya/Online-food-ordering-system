b934b232bdaf56637467e7b5ddda899bb96a5410
neojarvis <techadmin@iamneo.ai>
2025-05-10 12:37:20 +0000

    Auto-save-commit-10/05/2025--18:07:18

--- restaurant/src/main/java/com/examly/service/MenuServiceImpl.java
+++ restaurant/src/main/java/com/examly/service/MenuServiceImpl.java
@@ -7,29 +7,31 @@ import java.sql.SQLException;
 
 import com.examly.entity.Customer;
 import com.examly.exception.EmailAlreadyRegisteredException;
+import com.examly.exception.RestaurantNotFoundException;
 import com.examly.util.DBConnectionUtil;
 
 public class MenuServiceImpl implements MenuService{
     @Override
-    public boolean createCustomer(Customer customer) throws EmailAlreadyRegisteredException{
-        String checkRestaurant = "SELECT COUNT(*) FROM  WHERE email=?";
-        String insertQuery = "INSERT INTO customer (name, email, phoneNumber, password) VALUES(?,?,?,?)";
+    public boolean creatMenuItem(MenuItem menuItem) throws RestaurantNotFoundException{
+        String checkRestaurant = "SELECT COUNT(*) FROM restaurant WHERE restaurantId = ?";
+        String insertItem = "INSERT INTO menuItem (restaurantId, name, price, description, availableQuantity) VALUES(?, ?, ?, ?, ?)";
 
         try(Connection conn = DBConnectionUtil.getConnection();
-           PreparedStatement checkStmt = conn.prepareStatement(checkQuery);
-           PreparedStatement insertStmt = conn.prepareStatement(insertQuery)){ 
+           PreparedStatement checkStmt = conn.prepareStatement(checkRestaurant);
+           PreparedStatement insertStmt = conn.prepareStatement(insertItem)){ 
 
-           checkStmt.setString(1,customer.getEmail());
+           checkStmt.setInt(1,menuItem.getRestaurantId());
            ResultSet rs = checkStmt.executeQuery();
 
            if(rs.next() && rs.getInt(1) >0){
-            throw new EmailAlreadyRegisteredException("Email "+ customer.getEmail() + "is already registered");
+            throw new RestaurantNotFoundException("Restaurant with ID " + menuItem.getRestaurantId() + " not found");
            }
 
-           insertStmt.setString(1, customer.getName());
-           insertStmt.setString(2, customer.getEmail());
-           insertStmt.setString(3, customer.getPhoneNumber());
-           insertStmt.setString(4, customer.getPassword());
+           insertStmt.setInt(1, menuItem.getRestaurantId());
+           insertStmt.setString(2, menuItem.getName());
+           insertStmt.setDouble(3, menuItem.getPrice());
+           insertStmt.setString(4, menuItem.getDescription());
+           insertStmt.setInt(5, menuItem.getAvailableQuantity());
 
            return insertStmt.executeUpdate() > 0;
     }catch(SQLException e){
@@ -38,5 +40,15 @@ public class MenuServiceImpl implements MenuService{
     }
 }
 
+@Override
+public List<MenuItem> getMenuItemsByRestaurant(int restaurantId){
+    List<MenuItem> menuItems = new ArrayList<>();
+    String sql = "SELECT * FROM menuItem WHERE restaurantId = ?";
+    try(Connection conn = DBConnectionUtil.getConnection());
+       PreparedStatement stmt = conn.prepareStatement(sql)){
+        stmt.setInt(1, restaurantId)
+       }
+
+}
 }
 
