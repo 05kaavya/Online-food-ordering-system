13fe161d21260cb17e394dbfbbc8fa81a5cda3a6
neojarvis <techadmin@iamneo.ai>
2025-05-11 06:12:44 +0000

    Auto-save-commit-11/05/2025--11:42:39

--- restaurant/src/main/java/com/examly/service/RestaurantServiceImpl.java
+++ restaurant/src/main/java/com/examly/service/RestaurantServiceImpl.java
@@ -1,5 +1,64 @@
 package com.examly.service;
 
-public class RestaurantServiceImpl {
-    
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.examly.entity.Order;
+import com.examly.entity.Restaurant;
+import com.examly.util.DBConnectionUtil;
+
+public class RestaurantServiceImpl implements RestaurantService{
+    @Override
+    public boolean createRestaurant(Restaurant restaurant) {
+        String insertQuery = "INSERT INTO restaurant (name, address, cuisineType, contactNumber) VALUES(?, ?, ?, ?)";
+
+        try(Connection conn = DBConnectionUtil.getConnection();
+           PreparedStatement stmt = conn.prepareStatement(insertQuery)){
+           
+           stmt.setString(1, restaurant.getName());
+           stmt.setString(2, restaurant.getAddress());
+           stmt.setString(3, restaurant.getCuisineType());
+           stmt.setString(4, restaurant.getContactNumber());
+           
+
+           return stmt.executeUpdate() > 0;
+    }catch(SQLException e){
+        e.printStackTrace();
+        return false;
+    }
+}
+
+ @Override
+    public List<Restaurant> getAllRestaurants(){
+    List<Restaurant> restaurants = new ArrayList<>();
+    String sql = "SELECT * FROM restaurant";
+
+    try(Connection conn = DBConnectionUtil.getConnection();
+       Statement stmt = conn.createStatement();
+        
+        ResultSet rs = stmt.executeQuery(sql)){ 
+        while(rs.next()){
+            Order order = new Order(
+                rs.getInt("OrderId"),
+                rs.getInt("customerId"),
+                rs.getInt("restaurantId"),
+                rs.getString("orderStatus"),
+                rs.getDouble("totalPrice"),
+                rs.getInt("deliveryAddress")
+                );
+
+            orders.add(order);    
+            
+        }
+       }catch(SQLException e){
+        e.printStackTrace();
+       }
+       return orders;
+        
+    }
 }
