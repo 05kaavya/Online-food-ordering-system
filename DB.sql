cb914c9d594e58d329071a7827fedd357851428f
neojarvis <techadmin@iamneo.ai>
2025-05-13 05:43:53 +0000

    Submit-project-save-commit-13/05/2025--11:13:53

--- DB.sql
+++ DB.sql
@@ -3,5 +3,29 @@ use appdb;
 
 CREATE TABLE customer(
     customerId INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100)
-)
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phoneNumber VARCHAR(15),
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE restaurant(
+    restaurantId INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    cuisineType VARCHAR(55),
+    contactNumber VARCHAR(15) 
+);
+
+CREATE TABLE menuItem(
+    itemId INT PRIMARY KEY AUTO_INCREMENT,
+    restaurantId INT,
+    name VARCHAR(100) NOT NULL,
+    price DOUBLE NOT NULL,
+    description VARCHAR(255),
+    availableQuantity INT NOT NULL,
+    FOREIGN KEY (restaurantId) REFERENCES restaurant(restaurantId)
+
+);
+
+CREATE TABLE
